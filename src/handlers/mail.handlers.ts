cd1c310ccb4737c8f437636a9a9c4e3a53c19251
Thet Aung <thetaung.dev@gmail.com>
2023-11-02 17:42:21 +0300

    feat: added ip address to the form

--- src/handlers/mail.handlers.ts
+++ src/handlers/mail.handlers.ts
@@ -3,8 +3,9 @@ import { Request, Response } from "express";
 import { sendContactMail } from "../mail";
 
 export const contactEmail = async (req: Request, res: Response): Promise<any> => {
+
     try {
-        await sendContactMail({ ...req.body });
+        await sendContactMail({ ...req.body, ipAddress: req.socket.remoteAddress });
 
         return res.send({ message: "Success" });
     } catch (e) {
