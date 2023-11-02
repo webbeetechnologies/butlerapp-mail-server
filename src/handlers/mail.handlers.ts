310549bb10da1591f68b34371f1220ff3c58150a
Thet Aung <thetaung.dev@gmail.com>
2023-11-02 17:52:18 +0300

    chore: fixed ip address, formatted json

--- src/handlers/mail.handlers.ts
+++ src/handlers/mail.handlers.ts
@@ -3,9 +3,12 @@ import { Request, Response } from "express";
 import { sendContactMail } from "../mail";
 
 export const contactEmail = async (req: Request, res: Response): Promise<any> => {
-
+console.log({ ip: req.ip})
     try {
-        await sendContactMail({ ...req.body, ipAddress: req.socket.remoteAddress });
+        await sendContactMail({ ...req.body,
+            ipAddress: req.ip,
+            xForwardedFor: req.headers['x-forwarded-for'] || req.socket.remoteAddress
+        });
 
         return res.send({ message: "Success" });
     } catch (e) {
