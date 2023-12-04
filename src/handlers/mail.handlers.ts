80429e2de6fe842ea471881feaa8a9a225b67f34
Thet Aung <thetaung.dev@gmail.com>
2023-12-04 15:31:52 +0300

    fix: 500 server error issue

--- src/handlers/mail.handlers.ts
+++ src/handlers/mail.handlers.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import axios from 'axios'
+// import axios from 'axios'
 
 import { sendContactMail } from "../mail";
 import { MATTERMOST_MAIL_BOT_ACCESS_TOKEN } from "@/utils/constants";
 
-const MATTERMOST_INBOUNDS_CHANNEL_ID = 'pp9amtzhebdy8bi7ikz6m3jjgw';
+// const MATTERMOST_INBOUNDS_CHANNEL_ID = 'pp9amtzhebdy8bi7ikz6m3jjgw';
 
 export const contactEmail = async (req: Request, res: Response): Promise<any> => {
     try {
@@ -13,14 +13,14 @@ export const contactEmail = async (req: Request, res: Response): Promise<any> =>
             xForwardedForIP: req.headers['x-forwarded-for'] || req.socket.remoteAddress
         });
 
-        await axios.post('https://mattermost.bambooapp.ai/api/v4/posts', {
-            "channel_id": MATTERMOST_INBOUNDS_CHANNEL_ID,
-            "message": req.body
-        }, {
-            headers: {
-                Authorization: `Bearer ${MATTERMOST_MAIL_BOT_ACCESS_TOKEN}`
-            }
-        })
+        // await axios.post('https://mattermost.bambooapp.ai/api/v4/posts', {
+        //     "channel_id": MATTERMOST_INBOUNDS_CHANNEL_ID,
+        //     "message": req.body
+        // }, {
+        //     headers: {
+        //         Authorization: `Bearer ${MATTERMOST_MAIL_BOT_ACCESS_TOKEN}`
+        //     }
+        // })
 
         return res.send({ message: "Success" });
     } catch (e) {
