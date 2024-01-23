6d3fa4017bbfe41c683267529de92b1669fba6fa
Elvis Duru <duruelvis95@gmail.com>
2024-01-23 15:59:37 +0300

    add: libphonenumber-js verification

--- src/handlers/sms.handler.ts
+++ src/handlers/sms.handler.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import parsePhoneNumber from "libphonenumber-js/max";
 
 import { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER } from "@/utils/constants";
 
@@ -6,6 +7,11 @@ const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 export const sendSMS = async (req: Request, res: Response): Promise<any> => {
     try {
+        // Validate phone number first
+        if (!parsePhoneNumber(req.body.to).isValid()) {
+            return res.status(400).send({ message: "Invalid phone number" });
+        }
+
         const response = await client.messages.create({
             body: req.body.body,
             from: TWILIO_PHONE_NUMBER,
